9cfd86e7e708242d5b13426131974cb44deff25e
Harshita0801 <72064793+Harshita0801@users.noreply.github.com>
2022-11-19 20:43:02 +0530

    Delete sqlite.py

--- Assignments/Harshita L/assignment-1/sqlite.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn=sqlite3.connect("auth.db")
-print("Database opended successfully")
-
-conn.execute("Create Table table1 (name TEXT,mailid TEXT, password TEXT)")
-
-print("created successfully")
-conn.close()
